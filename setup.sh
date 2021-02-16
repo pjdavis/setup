a0980ff001116057f61173c04fd2c3e1e344f05f
PJ Davis <pj.davis@gmail.com>
2021-02-15 22:32:45 -0500

    Use iTerm 2

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 cd ~ 
+#Firefox
 if [ ! -f /Applications/Firefox.app/Contents/MacOS/firefox ]; then 
   echo "Installing Firefox"
   curl -L -o FirefoxSetup.dmg "https://download.mozilla.org/?product=firefox-latest&os=osx&lang=en-US"
@@ -6,4 +7,11 @@ if [ ! -f /Applications/Firefox.app/Contents/MacOS/firefox ]; then
   cp -R /Volumes/Firefox/Firefox.app /Applications
   umount /Volumes/Firefox
   rm FirefoxSetup.dmg
+fi
+# iTerm2
+if [ ! -f /Applications/iTerm.app/Contents/MacOS/iTerm2 ]; then
+  echo "Installing iTerm"
+  curl -L -o iTerm.zip "https://iterm2.com/downloads/stable/iTerm2-3_4_4.zip"
+  unzip iTerm.zip
+  mv iTerm /Applications
 fi
