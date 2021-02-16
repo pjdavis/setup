91a21b369a646b42a0c817f13206d160a9067782
PJ Davis <pj.davis@gmail.com>
2021-02-15 22:25:03 -0500

    Got to use curl

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 cd ~ 
 if [ ! -f /Applications/Firefox.app/Contents/MacOS/firefox ]; then 
   echo "Installing Firefox"
-  wget -O FirefoxSetup.dmg "https://download.mozilla.org/?product=firefox-latest&os=osx&lang=en-US"
+  curl -L -o FirefoxSetup.dmg "https://download.mozilla.org/?product=firefox-latest&os=osx&lang=en-US"
   hdiutil attach FirefoxSetup.dmg
   cp -R /Volumes/Firefox/Firefox.app /Applications
   rm FirefoxSetup.dmg
